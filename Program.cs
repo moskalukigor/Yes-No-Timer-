362fb8eeda466766ced699605810e1228a2ebcfd
moskalukigor <moskalukigor@mail.ru>
2015-01-25 18:20:40 +0300

    AllFiles

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; 
+
+namespace Yes_No_Timer_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int time = r.Next(2000,5000);
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            while (st.ElapsedMilliseconds <= time) { }
+            st.Stop();
+            st = Starttimer();
+            Console.WriteLine(st.Elapsed.ToString());
+        }
+        static Stopwatch Starttimer()
+        {
+            Console.WriteLine("s");
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            Console.ReadKey(true);
+            st.Stop();
+            
+            return st;
+        }
+    }
+}
